18d004910b518046534001af5d544a38fd6943bd
Jwestbellarmine <jwest4@bellarmine.edu>
2024-04-10 13:44:49 -0400

    initial commit

---
+++ gitDemo.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jayden West
+ */
+public class gitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Dr. Johnson! This is Jayden.");
+	}
+
+}
